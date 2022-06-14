b95e04c1e050aeb20a842b9a11988e873373b03d
prashantch265 <prashantchy265@gmail.com>
2022-06-14 07:48:20 +0545

    [wip] crud

---
+++ data/srms/section.data.js
@@ -0,0 +1,28 @@
+const { Section } = require("../../database/models/");
+
+const findOneByField = async (where) => {
+  where = { ...where, isActive: true, isDeleted: false };
+  return await Section.findOne({ where });
+};
+
+const add = async (data) => {
+  return await Section.create(data);
+};
+
+const update = async (data, id) => {
+  return await Section.update(data, { where: { id: id } });
+};
+
+const remove = async (id) => {
+  await Section.update(
+    { isActive: false, isDeleted: true },
+    { where: { id: id } }
+  );
+  return id;
+};
+
+const fetchAll = async () => {
+  return await Section.findAll({ where: { isActive: true, isDeleted: false } });
+};
+
+module.exports = { findOneByField, add, update, fetchAll, remove };
--- data/srms/students.data.js
+++ data/srms/students.data.js
@@ -2,6 +2,26 @@ const db = require("../../lib/sequelize");
 const { QueryTypes } = require("sequelize");
 const { Student } = require("../../database/models");
 
+const getAllQuery = `select
+students.id , students.name as "name", batch.name as "batch", semester.display_name as "semester", section.name as "section", students.user_name as "userName"
+from students
+inner join batch on batch.id = students.batch_id and batch.is_active is true
+inner join semester on semester.id = students.semester_id and semester.is_active is true
+left join section on section.id = students.section_id and section.is_active is true
+where students.is_active = true and students.is_deleted = false`;
+
+const getByIdQuery = `select
+students.id , students.name as "name", batch.name as "batch", semester.display_name as "semester", section.name as "section", 
+students.user_name as "userName", students.email, students.contact_no as "contact", students.date_of_birth as "dob", 
+students.gender , students.fathers_name as "fathersName", students.mothers_name as "mothersName", students.guardians_name as "guardiansName", 
+students.fathers_contact_no as "fathersContactNo", students.mothers_contact_no as "mothersContactNo", students.guardians_contact_no as "guardiansContactNo", 
+students.parents_email as "parentsEmail"
+from students
+inner join batch on batch.id = students.batch_id and batch.is_active is true
+inner join semester on semester.id = students.semester_id and semester.is_active is true
+left join section on section.id = students.section_id and section.is_active is true
+where students.is_active = true and students.is_deleted = false and students.id = $1`;
+
 const findOneByField = async (where) => {
   where = { ...where, isActive: true, isDeleted: false };
   return await Student.findOne({ where });
@@ -15,9 +35,28 @@ const updateStudentDetails = async (data, id) => {
   return await Student.update(data, { where: { id: id } });
 };
 
-const findAll = async () => {};
+const findAll = async () => {
+  return await db.sequelize.query(getAllQuery, { type: QueryTypes.SELECT });
+};
 
-const findById = async () => {};
+const findBySemester = async (semId) => {
+  const replacements = [];
+  const whereQuery = ` and students.semester_id = $1 `;
+  replacements.push(semId);
+  return await db.sequelize.query(getAllQuery + whereQuery, {
+    bind: replacements,
+    type: QueryTypes.SELECT,
+  });
+};
+
+const findById = async (id) => {
+  const replacements = [];
+  replacements.push(id);
+  return await db.sequelize.query(getByIdQuery, {
+    bind: replacements,
+    type: QueryTypes.SELECT,
+  });
+};
 
 const deleteStudentDetails = async (id) => {
   return await Student.update(
@@ -30,6 +69,7 @@ module.exports = {
   addStudentDetails,
   updateStudentDetails,
   findAll,
+  findBySemester,
   findById,
   deleteStudentDetails,
 };
